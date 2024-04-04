901afe7c4726458df52b1a361f2eea37ffc0043e
rushi-823744 <69962174+rushi-823744@users.noreply.github.com>
2024-04-04 14:04:23 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-def add(a,b):
-    return a+b
 
-print(add(1,2))
+Example :
+Take an User Input: 'k' = 66711
+
+Output: 9 12 21
+
+Input format :
+ The first line contains a number  'k'.
+Output format:
+ two sums sperated by odd & even sum & total value 
